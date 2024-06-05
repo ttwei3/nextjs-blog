90ffb3ce926511805e51d1cf14ffbec5c3ff214a
Tingting Wei <twei3@academic.rrc.ca>
2024-06-05 11:46:18 -0500

    add dummy data for posts

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,10 +1,11 @@
 import classes from './featured-posts.module.css';
+import PostsGrid from '../posts-grid';
 
-function FeaturedPosts() {
+function FeaturedPosts(props) {
   return (
     <section className={classes.latest}>
       <h2>Featured Posts</h2>
-      
+      <PostsGrid posts={props.posts} />
     </section>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,35 @@
+import FeaturedPosts from '@/components/home-page/featured-posts';
 import Hero from '../components/home-page/hero';
 import { Fragment } from 'react';
+
+const DUMMY_POSTS = [
+  { slug: 'getting-started-with-nextjs1',
+  title: 'getting-started-with-nextjs',
+  image: 'getting-started-with-nextjs.png',
+  excerpt: 'getting-started-with-nextjs',
+  date: '2024-06-05' },
+  { slug: 'getting-started-with-nextjs2',
+  title: 'getting-started-with-nextjs',
+  image: 'getting-started-with-nextjs.png',
+  excerpt: 'getting-started-with-nextjs',
+  date: '2024-06-05' },
+  { slug: 'getting-started-with-nextjs3',
+  title: 'getting-started-with-nextjs',
+  image: 'getting-started-with-nextjs.png',
+  excerpt: 'getting-started-with-nextjs',
+  date: '2024-06-05' },
+  { slug: 'getting-started-with-nextjs4',
+  title: 'getting-started-with-nextjs',
+  image: 'getting-started-with-nextjs.png',
+  excerpt: 'getting-started-with-nextjs',
+  date: '2024-06-05' },
+];
 
 function HomePage() {
   return (
     <Fragment>
       <Hero />
+      <FeaturedPosts posts={DUMMY_POSTS}/>
     </Fragment>
   );
 }
