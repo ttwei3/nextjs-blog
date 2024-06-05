e7c3e1901aa03cb658333e6c22afd8f5ba2bbdce
Tingting Wei <twei3@academic.rrc.ca>
2024-06-05 12:12:49 -0500

    add responsive layout for image

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -13,14 +13,13 @@ function PostItem(props) {
 
   const imagePath = `/images/posts/${slug}/${image}`;
   const linkPath = `/posts/${slug}`;
-  console.log('Image Path:', imagePath); // 添加这一行用于调试
 
 
   return(
     <li className={classes.post}>
       <Link href={linkPath}>
         <div className={classes.image}>
-          <Image src={imagePath} alt={title} width={300} height={200} />
+          <Image src={imagePath} alt={title} width={300} height={200} layout='responsive'/>
         </div>
         <div className={classes.content}>
           <h3>{title}</h3>
